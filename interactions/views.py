cdbe07b415c662bcefb110461e0103cbafeabce0
Suganth <g.suganth8270@gmail.com>
2026-07-09 12:18:00 +0530

    Add interaction search and filtering

--- interactions/views.py
+++ interactions/views.py
@@ -7,19 +7,37 @@ from .serializers import InteractionSerializer
 
 @api_view(["GET"])
 def interaction_list(request):
-    """
-    Return all protein–biomaterial interactions.
-    """
+    protein = request.GET.get("protein")
+    biomaterial = request.GET.get("biomaterial")
+    interaction = request.GET.get("interaction")
+
     interactions = Interaction.objects.all()
+
+    if protein:
+        interactions = interactions.filter(
+            protein__protein_name__icontains=protein
+        )
+
+    if biomaterial:
+        interactions = interactions.filter(
+            biomaterial__name__icontains=biomaterial
+        )
+
+    if interaction:
+        interactions = interactions.filter(
+            interaction_type__icontains=interaction
+        )
+
     serializer = InteractionSerializer(interactions, many=True)
     return Response(serializer.data)
+
 
 @api_view(["GET"])
 def interaction_detail(request, pk):
     try:
         interaction = Interaction.objects.get(pk=pk)
     except Interaction.DoesNotExist:
+          
         return Response({"error": "Interaction not found"}, status=404)
-
     serializer = InteractionSerializer(interaction)
     return Response(serializer.data)
