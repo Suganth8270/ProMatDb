a071a8bd9f39b764f5c8d4eba9e8172ba0280d88
Suganth <g.suganth8270@gmail.com>
2026-07-09 12:51:44 +0530

    Add nested serializers for interaction API

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
+
 from .models import Interaction
+from proteins.models import Protein
+from biomaterials.models import Biomaterial
+
+
+class ProteinMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Protein
+        fields = ["id", "protein_name", "uniprot_id"]
+
+
+class BiomaterialMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Biomaterial
+        fields = ["id", "name", "category"]
 
 
 class InteractionSerializer(serializers.ModelSerializer):
+    protein = ProteinMiniSerializer(read_only=True)
+    biomaterial = BiomaterialMiniSerializer(read_only=True)
+
     class Meta:
         model = Interaction
         fields = "__all__"
